de6fd77fc6d173e636e5743ec031aaa108a23d40
K4n3dam4 <64373789+K4n3dam4@users.noreply.github.com>
2022-08-25 16:33:28 +0200

    refactor(env): move envs to root, feat(api-e2e): add api testing against test db

---
+++ apps/api-e2e/jest.config.ts
@@ -0,0 +1,18 @@
+/* eslint-disable */
+export default {
+  name: 'api-e2e',
+  preset: '../../jest.preset.js',
+  rootDir: '.',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  testMatch: ['**/+(*.)+(e2e-spec|e2e-test).+(ts|js)?(x)'],
+  coverageDirectory: '../../coverage/apps/api/e2e',
+  moduleFileExtensions: ['ts', 'js', 'html'],
+};
---
+++ apps/api-e2e/src/app.e2e-spec.ts
@@ -0,0 +1,9 @@
+import * as pactum from 'pactum';
+
+describe('Api e2e', () => {
+  describe('e2e running...', () => {
+    it('should return true', () => {
+      // run
+    });
+  });
+});
