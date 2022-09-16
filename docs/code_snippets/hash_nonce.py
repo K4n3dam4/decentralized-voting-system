f81445a165418ab5ee1b13fbb55d92330fcaae00
K4n3dam4 <64373789+K4n3dam4@users.noreply.github.com>
2022-09-16 16:20:46 +0200

    chore(thesis): adds pow

--- docs/code_snippets/hash_nonce.py
+++ docs/code_snippets/hash_nonce.py
@@ -1,25 +1,23 @@
 # /************************************************
-#   Title: A script for generating many hashes by iterating on a nonce
+#   Based on
+#   Title: Generating many hashes by iterating on a nonce
 #   Author: Antonopoulos, A
 #   Date: 2017
 #   Code version: 1.0.0
 #   Availability: O'Reilly
 # *************************************************
 
-# example of iterating a nonce in a hashing algorithm's input
-
 import hashlib
 
-text = "I am Satoshi Nakamoto"
+input_value = "A nonce will be added to this value"
 
 # iterate nonce from 0 to 19
 for nonce in xrange(20):
+    # add nonce to the input value
+    input_with_nonce = input_value + str(nonce)
 
-    # add the nonce to the end of the test
-    input = text + str(nonce)
+    # calculate the SHA-256 hash of the input value (input_value+nonce)
+    hashed_value = hashlib.sha256(input_with_nonce).hexdigest()
 
-    # calculate the SHA-256 hash of the input (text+nonce)
-    hash = hashlib.sha256(input).hexdigest()
-
-    # show the input and hash result
-    print input, '=>', hash
+    # print input value and hash result
+    print input_with_nonce, '=>', hashed_value
