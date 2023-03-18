7782cfe2ea229b9a9ea0caab27704dc2a014659d
K4n3dam4 <64373789+K4n3dam4@users.noreply.github.com>
2023-03-18 14:27:41 +0100

    refactor(client) remove logo placeholder

--- apps/client/components/molecules/DVSNavbar.tsx
+++ apps/client/components/molecules/DVSNavbar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Box, Button, Flex, Heading, Stack, useColorMode, useColorModeValue, useMediaQuery } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
-import Link from 'next/link';
 import DVSLink, { DVSLinkProps } from '../atoms/DVSLink';
 
 export interface DVSNavbarProps {
@@ -19,10 +18,7 @@ const DVSNavbar: React.FC<DVSNavbarProps> = ({ links, children, title }) => {
       <Box position="fixed" width="100%" zIndex={1000} bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={16} alignItems="center" justifyContent="space-between">
           <Stack direction="row" spacing={8} alignItems="center">
-            <Stack direction="row" spacing={5}>
-              <Link href="/">Logo</Link>
-              {title && <Heading size="md">{displayName && title}</Heading>}
-            </Stack>
+            {title && <Heading size="md">{displayName && title}</Heading>}
             <Stack direction="row" spacing={5}>
               {links &&
                 links.map(({ children, ...restProps }) => (
